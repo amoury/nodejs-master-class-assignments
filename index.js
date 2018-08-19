d1c681a44dd814efbfb3455037c2076b1f3b938f
Amoury <ansar.memon@hotmail.com>
2018-08-19 11:53:55 +0400

    Added CRUD functions for operations using file system using Async/Await

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 
+const handlers = require('./lib/handlers');
+const _data = require('./lib/data');
+
+
 const httpServer = http.createServer((req, res) => serverOps(req, res));
 
 
@@ -20,23 +24,23 @@ const serverOps = (req, res) => {
   const headers = req.headers;
 
   // 4. Get the payload
-  getPayload(req, res)
-    .then(result => {
-      // 5. Handle Routing
-      const chosenHandler = typeof router[path] !== "undefined" ? router[path] : handlers.notFound;
-      const data = { path, queryString, method, headers, payload: result };
-      
-      chosenHandler(data, (statusCode, payload) => {
-        statusCode = typeof statusCode == "number" ? statusCode : 200;
-        payload = typeof payload == "object" ? payload: {};
-        const payloadString = JSON.stringify(payload); 
+  let payload = async () => {
+    const result = await getPayload(req, res);
+    const chosenHandler = typeof router[path] !== "undefined" ? router[path] : handlers.notFound;
+    const data = { path, queryString, method, headers, payload: result };
 
-        res.setHeader("Content-Tyype", "application/json");
-        res.writeHead(statusCode);
-        res.end(payloadString);
-      })
-      
-    });
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
+      payload = typeof payload == "object" ? payload: {};
+      const payloadString = JSON.stringify(payload); 
+
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    })  
+  };
+
+  payload(req, res);
   };
   
   
@@ -62,20 +66,9 @@ const getPayload = (req, res) => {
 
 
 
-
-// Route Handler
-const handlers = {};
-handlers.hello = (data, callback) => {
-  callback(200, {'message': 'Hello and Welcome to Node JS'});
-}
-
-// 404 Handler
-handlers.notFound = (data, callback) => {
-  callback(404);
-}
-
 const router = {
-  hello: handlers.hello
+  'hello': handlers.hello,
+  'users': handlers.users
 }
 
 
